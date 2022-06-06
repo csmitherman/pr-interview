08d02d046d733e1986aa30c8c92fee24c18c3003
Christian Smithermamn <hello@christiansmitherman.com>
2022-06-06 17:20:34 -0400

    revert

--- scripts.js
+++ scripts.js
@@ -1,27 +1,13 @@
 var close = document.querySelector("#close-modal");
 
 close.addEventListener("click", function () {
-    console.log("testing");
     var modal = document.querySelector("#modal");
     modal.classList.remove("visible");
 });
 
-var username = document.querySelector("#username");
-var password = document.querySelector("#password");
-
-// Change aria-labelledby to aria-label
-username.setAttribute("aria-label", "username-label");
-password.setAttribute("aria-label", "password-label");
-
 var forgotPassword = document.querySelector("#forgot-password");
 
-forgotPassword.setAttribute("role", "button");
-
-var footerMenu = document.querySelector("#footer-menu");
-var menuItems = document.querySelectorAll("#footer-menu li");
-
-footerMenu.setAttribute("role", "menubar");
-
-menuItems.forEach(function (item) {
-    item.setAttribute("role", "menuitem")
+forgotPassword.addEventListener("click", function () {
+    var modal = document.querySelector("#modal");
+    modal.classList.add("visible");
 });
