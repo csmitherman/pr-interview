065fff0ffc47fdaefc3eb3d4c893b72648ca0c53
Christian Smithermamn <hello@christiansmitherman.com>
2022-06-06 17:14:16 -0400

    awesome update to branch

--- scripts.js
+++ scripts.js
@@ -1,13 +1,27 @@
 var close = document.querySelector("#close-modal");
 
 close.addEventListener("click", function () {
+    console.log("testing");
     var modal = document.querySelector("#modal");
     modal.classList.remove("visible");
 });
 
+var username = document.querySelector("#username");
+var password = document.querySelector("#password");
+
+// Change aria-labelledby to aria-label
+username.setAttribute("aria-label", "username-label");
+password.setAttribute("aria-label", "password-label");
+
 var forgotPassword = document.querySelector("#forgot-password");
 
-forgotPassword.addEventListener("click", function () {
-    var modal = document.querySelector("#modal");
-    modal.classList.add("visible");
+forgotPassword.setAttribute("role", "button");
+
+var footerMenu = document.querySelector("#footer-menu");
+var menuItems = document.querySelectorAll("#footer-menu li");
+
+footerMenu.setAttribute("role", "menubar");
+
+menuItems.forEach(function (item) {
+    item.setAttribute("role", "menuitem")
 });
